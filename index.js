1ae295abf93d70acf354bfd8cb212576c6be7f33
Toast <tostertonyacount@gmail.com>
2025-02-22 13:54:29 +0200

    index.js

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const http = require('http')
 const fs = require('fs')
 const path = require('path')
 const dataPath = path.join(__dirname, 'data')
+const url = require('url')
 
 const server = http.createServer((req, res) => {
-    if (req.url=='/' && req.method == 'GET'){
+    if (req.url=='/jokes' && req.method == 'GET'){
         getJokes(req, res)
+    }
+    if (req.url=='/jokes' && req.method == 'POST'){
+        postJoke(req, res)
+    }
+    if (req.url.startsWith('/like')){
+        like(res, req)
+    }
+    if (req.url.startsWith('/dislike')){
+        dislike(res, req)
     }
 })
 server.listen(3000)
@@ -23,3 +33,57 @@ function getJokes(req, res){
     }
     res.end(JSON.stringify(jokes))
 }
+function postJoke(req, res){
+    let data = ''
+    req.on(data, function(chunk){
+        data += chunk
+    })
+    req.on(end, function(){
+        let joke = JSON.parse(data)
+        joke.likes = 0
+        joke.dislikes = 0
+
+        let dir = fs.readFileSync(dataPath)
+        let filename = dir.length+length
+        let filePath = path.join(dataPath, filename)
+        fs.writeFileSync(filePath, JSON.stringify(joke))
+        res.end()
+    })
+}
+
+function like(req, res){
+    const params = url.parse(req.url, true).query
+    let id = params.id
+    console.log(id)
+    if (id){
+        let filePath = path.join(dataPath, id+'.json')
+        let file = fs.readFileSync(filePath)
+
+        let jokeJSON = Buffer.from(file).toString()
+        let joke = JSON.parse(jokeJSON)
+
+        joke.likes ++
+
+        fs.writeFileSync(filePath, JSON.stringify(joke))
+    }
+    res.end()
+}
+
+function dislike(req, res){
+    const params = url.parse(req.url, true).query
+    let id = params.id
+    console.log(id)
+    if (id){
+        let filePath = path.join(dataPath, id+'.json')
+        let file = fs.readFileSync(filePath)
+
+        let jokeJSON = Buffer.from(file).toString()
+        let joke = JSON.parse(jokeJSON)
+
+        joke.dislikes ++
+
+        fs.writeFileSync(filePath, JSON.stringify(joke))
+    }
+    res.end()
+}
+
